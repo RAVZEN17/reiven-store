dedc0cc906aea2cf7a23b0b50821a78c51a48c8d
Juan Morales Vargas <ancajima2308@gmail.com>
2025-12-12 14:53:16 -0500

    Implement loader display on page load
    Add loader functionality to display content after loading.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-﻿console.log("Reiven Store funcionando.");
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("loader").style.opacity = "0";
+        setTimeout(() => {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        }, 500);
+    }, 1500);
+});
